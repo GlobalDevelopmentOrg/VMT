aa641b86395d2d58aa679f4b0745c765dcb5db9b
JustDaile <daile.alimo1991@hotmail.com>
2018-02-02 10:25:57 +0000

    Setup Maven and JUnit

---
+++ src/main/java/fort/of/fri/AppLauncher.java
@@ -0,0 +1,21 @@
+package fort.of.fri;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @Author
+ */
+public class App extends Application {
+
+    // think of stage and the main app window
+    public void start(Stage stage){
+        // TODO init main window
+    }
+
+    public static void main(String[] args) {
+        new App().launch(args);
+    }
+
+}
--- src/test/java/fort/of/fri/AppLauncherTest.java
+++ src/test/java/fort/of/fri/AppLauncherTest.java
@@ -11,6 +11,6 @@ import junit.framework.TestCase;
  */
 public class AppTest extends TestCase {
 
-    
+
 
 }
