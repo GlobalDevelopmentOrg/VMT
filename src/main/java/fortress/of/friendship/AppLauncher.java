3f7847e850d270a44acfa21ac2a7f64fd3549d33
JustDaile <daile.alimo1991@hotmail.com>
2018-02-02 15:02:19 +0000

    Started working on h2 embedded database connector class, added tests to ensure database connector works correctly

--- src/main/java/fortress/of/friendship/AppLauncher.java
+++ src/main/java/fortress/of/friendship/AppLauncher.java
@@ -1,4 +1,4 @@
-package fort.of.fri;
+package fortress.of.friendship;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
         Don't forget to comment, keep track of your changes so you can explain
         why you changed them and the problem these changes addressed.
  */
+
 /**
  * Entry point for this application
  *
@@ -17,7 +18,7 @@ import javafx.stage.Stage;
 public class AppLauncher extends Application {
 
     // think of stage and the main app window
-    public void start(Stage stage){
+    public void start(Stage stage) {
         // TODO init main window
     }
 
