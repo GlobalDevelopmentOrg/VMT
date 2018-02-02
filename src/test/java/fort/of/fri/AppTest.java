6d5c82db908c062e1dd6e804cb57b3f60764b0c5
JustDaile <daile.alimo1991@hotmail.com>
2018-02-02 10:11:51 +0000

    Setup maven and JUnit

---
+++ src/test/java/fort/of/fri/AppTest.java
@@ -0,0 +1,16 @@
+package fort.of.fri;
+
+import junit.framework.TestCase;
+
+/**
+ *   Unit tests ensure we run tests before making a pull request.
+ *   All tests should run.
+ *
+ *   Write tests for new classes you create,
+ *   this is ensure other developers don't break them.
+ */
+public class AppTest extends TestCase {
+
+    
+
+}
