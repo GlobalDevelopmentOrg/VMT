824662f047abed91427e34f0a2c430c56ec0deea
justdaile <daile.alimo1991@hotmail.com>
2018-02-06 03:40:03 +0000

    Immutable

--- src/main/java/vehicle/maintenance/tracker/models/Part.java
+++ src/main/java/vehicle/maintenance/tracker/models/Part.java
@@ -8,61 +8,48 @@ import java.util.Date;
  * maintenance schedules. 
  *
  * @author add your name
+ * @author Daile Alimo
  * @since 0.1-SNAPSHOT
  */
 public class Part {
 
     private int id;
+    private vehicleId; // id of vehicle this part belongs too
     private Date installDate;
     private Date scheduledMaintenanceDate;
     private String name;
 
-    public Part() {
-        this.id = 0;
-        this.installDate = new Date();
-	// TODO calculate or allow client to set a scheduled maintenants date
-	// for now just instantiate it to prevent null pointer errors.
-	this.scheduledMaintenanceDate = this.installDate;
-        this.name = "none";
-    }
-
-    public Part(int id, Date installDate, Date maintainDate, String name) {
+    // we can use this when we have data from database.
+    public Part(int id, int vehicleId, Date installDate, Date scheduledMaintenanceDate, String name) {
         this.id = id;
+	this.vehicleId = vehicleId;
         this.installDate = installDate;
         this.scheduledMaintenanceDate = scheduledMaintenanceDate;
         this.name = name;
+    }
+
+    // we use this constructor id will be auto generated when we submit it to database.
+    public Part(int vehicleId, Date installDate, Date scheduledMaintenanceDate, String name){
+	this.vehicleId = vehicleId;
+	this.installDate = installDate;
+	this.scheduledMaintenanceDate = scheduledMaintenanceDate;
+	this.name = name; 
     }
 
     public int getid() {
         return this.id;
     }
 
-    public void setid(int id) {
-        this.id = id;
-    }
-
     public Date getinstallDate() {
         return this.installDate;
     }
 
-    public void setinstallDate(Date date) {
-        this.installDate = date;
-    }
-
-    public Date getmaintainDate() {
-        return this.maintainDate;
-    }
-
-    public void setmaintainDate(Date date) {
-        this.maintainDate = date;
+    public Date getScheduledMaintenanceDate() {
+        return this.scheduledMaintenanceDate;
     }
 
     public String getname() {
         return this.name;
-    }
-
-    public void setname(String name) {
-        this.name = name;
     }
 
 }
